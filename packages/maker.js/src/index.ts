79e42b0f67b8517517680d56d8fed2703b74e4ab
Myka White <mwhite454@gmail.com>
2025-10-04 00:30:52 -0400

    refactor(core): export path module from index, build successful with 3 modules

--- packages/maker.js/src/index.ts
+++ packages/maker.js/src/index.ts
@@ -5,8 +5,8 @@ export * from './core/maker.js';
 // Core modules converted to ES modules
 export * as angle from './core/angle.js';
 export * as point from './core/point.js';
+export * as path from './core/path.js';
 
 // TODO: progressively export remaining modules
-// export * as path from './core/path.js';
 // export * as measure from './core/measure.js';
 // export * as model from './core/model.js';

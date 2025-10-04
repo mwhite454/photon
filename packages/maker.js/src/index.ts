838da47702e3d3bd763ee946d604f5693837d5a6
Myka White <mwhite454@gmail.com>
2025-10-04 00:22:54 -0400

    refactor(core): export angle and point modules from index, build ES bundles successfully

--- packages/maker.js/src/index.ts
+++ packages/maker.js/src/index.ts
@@ -1,9 +1,12 @@
 // ES module entry point during migration.
 export * from './core/schema.js';
-export * from './core/base.js';
+export * from './core/maker.js';
 
-// TODO: progressively export concrete modules, e.g. angle, point, path, measure, etc.
-// export * as angle from './core/angle.js';
-// export * as point from './core/point.js';
+// Core modules converted to ES modules
+export * as angle from './core/angle.js';
+export * as point from './core/point.js';
+
+// TODO: progressively export remaining modules
 // export * as path from './core/path.js';
 // export * as measure from './core/measure.js';
+// export * as model from './core/model.js';
